063f4603a11e6abc8727b743f5f9e6d5e1b19f56
newlnwza <73693983+newlnwza@users.noreply.github.com>
2020-10-30 17:01:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     </head>
     <body>
         <h1>My First Heading</h1>
-        <p>My First Paragraph</p>
+        <p>My First Paragraph 2</p>
         <img src="cat.jpg">
         <br>
         <a href="http://www.it.kmitl.ac.th">This is a Link</a>
