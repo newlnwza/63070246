3b914eb391504396103085d9b0b64867d5728c32
newlnwza <73693983+newlnwza@users.noreply.github.com>
2020-11-13 17:32:33 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'panwit.mysql.database.azure.com', 'panwit@panwit', '123456789', 'itflab', 3306);
+mysqli_real_connect($conn, 'it63070246.mysql.database.azure.com', 'it63070246@it63070246', 'VGDsot23', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
