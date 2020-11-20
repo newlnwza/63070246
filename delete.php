76bf8dbcb4ef0ee2c13255a5328c83e791dbd587
newlnwza <73693983+newlnwza@users.noreply.github.com>
2020-11-20 19:48:42 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itfraiden.mysql.database.azure.com', 'raiden@itfraiden', 'Itf12644', 'itf63070222', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+$ID = $_POST['ID'];
+$sql = "DELETE FROM guestbook WHERE ID='$ID'";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record deleted successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
